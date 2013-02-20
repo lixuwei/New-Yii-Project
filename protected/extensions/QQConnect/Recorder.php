e86a6567fecd09e5a788b84e19658673bd944f64
Rogee <rogeecn@gmail.com>
2013-02-20 17:18:23 +0800

    添加QQ互联登录

---
+++ protected/extensions/QQConnect/Recorder.php
@@ -0,0 +1,59 @@
+<?php
+/* PHP SDK
+ * @version 2.0.0
+ * @author connect@qq.com
+ * @copyright © 2013, Tencent Corporation. All rights reserved.
+ */
+
+class Recorder{
+    private static $data;
+    private $inc;
+    private $error;
+
+    public function __construct(){
+        $this->error = new ErrorCase();
+
+        //-------读取配置文件
+        $incFileContents = file_get_contents(dirname(__FILE__)."/inc.php");
+        $this->inc = json_decode($incFileContents);
+        if(empty($this->inc)){
+            $this->error->showError("20001");
+        }
+
+        $session = Yii::app()->user->getState('QC_userData');
+        if(empty($session)){
+            self::$data = array();
+        }else{
+            self::$data = $session;//$_SESSION['QC_userData'];
+        }
+    }
+
+    public function write($name,$value){
+        self::$data[$name] = $value;
+    }
+
+    public function read($name){
+        if(empty(self::$data[$name])){
+            return null;
+        }else{
+            return self::$data[$name];
+        }
+    }
+
+    public function readInc($name){
+        if(empty($this->inc->$name)){
+            return null;
+        }else{
+            return $this->inc->$name;
+        }
+    }
+
+    public function delete($name){
+        unset(self::$data[$name]);
+    }
+
+    function __destruct(){
+        //$_SESSION['QC_userData'] = self::$data;
+        Yii::app()->user->setState('QC_userData', self::$data);
+    }
+}
